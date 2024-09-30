c5be899e0f67c0a3daa4f29ced455ed5c95ca655
ranyang <1840586637@qq.com>
2024-09-30 16:28:58 +0800

    complete

--- src/views/Tictactoe.tsx
+++ src/views/Tictactoe.tsx
@@ -71,21 +71,41 @@ const Board = ({xIsNext, squares, onPlay}: {xIsNext: boolean, squares: Array<str
 }
 
 const Game = () => {
-	const [xIsNext, setXIsNext] = useState(true)
 	const [history, setHistory] = useState([Array(9).fill(null)])
+	const [currentMove, setCurrentMove] = useState(0)
+	const xIsNext = (currentMove % 2) === 0
 
-	const currentSquares = history[history.length - 1]
+	const currentSquares = history[currentMove]
 	const handlePlay = (nextSquares: Array<string|null>)=> {
-		setHistory([...history, nextSquares])
-		setXIsNext(!xIsNext)
+		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+		setHistory(nextHistory)
+		setCurrentMove(nextHistory.length - 1)
 	}
+
+	const jumpTo = (nextMove: number) => {
+		setCurrentMove(nextMove)
+	}
+
+	const moves = history.map((_, move) => {
+		let description
+		if (move > 0) {
+			description = 'Go to move #' + move
+		} else {
+			description = 'Go to game start'
+		}
+		return (
+			<li key={move}>
+				<button onClick={() => jumpTo(move)}>{description}</button>
+			</li>
+		)
+	})
 	return (
 		<div className="game">
 			<div className="game-board">
 				<Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}></Board>
 			</div>
 			<div className="game-info">
-				<ol></ol>
+				<ol>{moves}</ol>
 			</div>
 		</div>
 	)
